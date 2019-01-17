3f7596baa7b1cbe4996319177fff0fd2071c019e
Doldrums <doldrums.317@gmail.com>
2019-01-17 18:35:37 +0200

    Initial commit

---
+++ app/src/main/java/com/example/doldr/kode_task_5/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.doldr.kode_task_5
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
